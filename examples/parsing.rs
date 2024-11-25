5c72149fd2928d7957be6e7ee18867e2b877563e
Lyss <shaitan.lyssm@gmail.com>
2024-11-25 23:32:03 +0100

    Add example

--- examples/parsing.rs
+++ examples/parsing.rs
@@ -1,3 +1,18 @@
-fn main() {
-    
+use geode::GeoscienceModel;
+use std::fs;
+
+fn parse_compositional_injection_basic() -> Result<(), Box<dyn std::error::Error>> {
+    println!(
+        "{:#?}",
+        fs::read_to_string("example_configs/compositional-injection-basic.yaml")?
+            .parse::<GeoscienceModel>()?
+    );
+
+    Ok(())
 }
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    parse_compositional_injection_basic()?;
+    Ok(())
+}
+
